e76565ce7cab508ebc13e1edcc7638b3526fbb93
eljam3239 <eljam3239@7KTM6Y1.Student.UGDSB.ED>
2017-02-28 13:26:38 -0500

    wayne gretzky jersey

---
+++ src/waynegretzkyinfo/WayneGretzkyInfo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package waynegretzkyinfo;
+
+/**
+ *
+ * @author eljam3239
+ */
+public class WayneGretzkyInfo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String wayne = "Wayne Gretzky's jersey number is ";
+        byte jerseyNumber =  99;
+        System.out.println(wayne + jerseyNumber);
+    }
+    
+}
